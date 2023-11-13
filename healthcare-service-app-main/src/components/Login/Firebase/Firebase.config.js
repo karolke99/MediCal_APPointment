fcba924e6e7fc47e30f50848395d4f0b7289f639
Gabriela Zapała <gabzapala@gmail.com>
2023-11-13 13:00:50 +0100

    frontend base

---
+++ healthcare-service-app-main/src/components/Login/Firebase/Firebase.config.js
@@ -0,0 +1,23 @@
+console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCj_t2shNT7ZP9uBpoCyQEMCfWrR7IhQfI",
+    authDomain: "healthcare-service-react.firebaseapp.com",
+    projectId: "healthcare-service-react",
+    storageBucket: "healthcare-service-react.appspot.com",
+    messagingSenderId: "27497610294",
+    appId: "1:27497610294:web:553178efba088d916cec97",
+    measurementId: "G-5BZ007JHHT"
+};
+
+
+export default firebaseConfig;
